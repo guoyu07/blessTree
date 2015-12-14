937cacdd4c10089646cb55bbee292d5582bcf03d
QC-Li <cc794058798@sina.cn>
2015-12-15 01:36:28 +0800

    authentic wechat

--- bTree/urls.py
+++ bTree/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from bTree.views import test, weixin_main
 urlpatterns = [
-    url(r'', test),
+    url(r'^$', test),
     url(r'^wechat/', weixin_main),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/$', include(admin.site.urls)),
 ]
