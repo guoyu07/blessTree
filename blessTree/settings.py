cdfa680c6e8c3c22dec4a58e0952e88b62af9510
QC-Li <cc794058798@sina.cn>
2015-12-14 18:03:06 +0800

    inti

--- blessTree/settings.py
+++ blessTree/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2w%+23)d-xu*#12^1rewlbj=&d_-!ncq*t=2-g@$2^80wro&q+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = [
     '.sinaapp.com',
