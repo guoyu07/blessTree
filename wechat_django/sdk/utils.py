522d99ec638eef0b47af776dd45b3be40d7a80e5
QC-Li <cc794058798@sina.cn>
2015-12-15 17:15:59 +0800

    encoding fix

--- wechat_django/sdk/utils.py
+++ wechat_django/sdk/utils.py
@@ -1,11 +1,11 @@
-# -*-coding:utf-8-*-
+# -*- coding:utf-8 -*-
 __author__ = 'albert'
 """
     wechat_django.sdk.utils:
     一些有用的函数
 """
 
-# from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import, unicode_literals
 import string
 import random
 import hashlib
@@ -61,23 +61,6 @@ def check_signature(token, signature, timestamp, nonce):
         # raise InvalidSignatureException()
         return False
     return True
-# class WeChatSigner(object):
-#     """wechat 数据签名,用于微信与服务器验证校验 """
-#     def __init__(self, delimiter=b''):
-#         self._data = []
-#         self._delimiter = to_binary(delimiter)
-#
-#     def add_data(self, *args):
-#         """ 把数据加到singer上"""
-#         for data in args:
-#             self._data.append(to_binary(data))
-#
-#     @property  # 这个内置的装饰器可以让函数像属性一样访问：WeChatSignerObj.signature
-#     def signature(self):
-#         """得到数据签名 """
-#         self._data.sort()
-#         str_to_sign = self._delimiter.join(self._data)  # 将列表转化为字符串，为了给hashlib.sha1处理
-#         return hashlib.sha1(str_to_sign).hexdigest()
 
 
 def to_binary(value, encoding='utf-8'):
@@ -95,30 +78,12 @@ def to_binary(value, encoding='utf-8'):
     return six.binary_type(value)
 
 
-# def check_signature(token, signature, timestamp, nonce):
-#     """ 检验微信服务器端回调过来的值
-#         用于微信服务器端与本服务器端的链接验证
-#
-#     :param token:微信回调的token，你自己定义的
-#     :param signature: 微信服务器生成的签名，在GET方法中可以得到
-#     :param timestamp: 微信服务器生成的，在GET方法中可以得到
-#     :param nonce: 微信服务器生成的，在GET方法中可以得到
-#     :return:None
-#     """
-#
-#     signer = WeChatSigner()
-#     signer.add_data(token, timestamp, nonce)
-#     if signer.signature != signature:
-#         return False
-#     return True
-
-
 def to_text(value, encoding='utf-8'):
     """ 将数据转换为文本，默认编码是utf-8
 
     :param value: 被转换的数据
     :param encoding: 编码规则
-    :return:text类型的数据
+    :return:unicode类型的数据
     """
     if not value:
         return ''
@@ -126,8 +91,7 @@ def to_text(value, encoding='utf-8'):
         return value
     if isinstance(value, six.binary_type):
         return value.decode(encoding)
-    # return six.text_type(value)
-    return value
+    return six.text_type(value)
 
 
 def timezone(zone):
