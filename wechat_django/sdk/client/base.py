3d248cf6ef695dd92191bfee4a2b7f9bc2419fe4
QC-Li <cc794058798@sina.cn>
2016-01-21 01:00:25 +0800

    test for fetch user-info

--- wechat_django/sdk/client/base.py
+++ wechat_django/sdk/client/base.py
@@ -1,4 +1,4 @@
-# -*-coding:uuf-8 -*-
+# -*-coding:utf-8 -*-
 __author__ = 'albert'
 
 """
@@ -7,24 +7,23 @@ __author__ = 'albert'
 
 
 class BaseWeChatAPI(object):
-    # def __init__(self, client=None):
-    #     self._client = client
-    #
-    # def _get(self, url, **kwargs):
-    #     if getattr(self, 'API_BASE_URL', None):
-    #         kwargs['api_base_url'] = self.API_BASE_URL
-    #     return self._client.get(url, **kwargs)
-    #
-    # def _post(self, url, **kwargs):
-    #     if getattr(self, 'API_BASE_URL', None):
-    #         kwargs['api_base_url'] = self.API_BASE_URL
-    #     return self._client.post(url, **kwargs)
-    #
-    # @property
-    # def access_token(self):
-    #     return self._client.access_token
-    #
-    # @property
-    # def session(self):
-    #     return self._client.session
-    pass
+    def __init__(self, client=None):
+        self._client = client
+
+    def _get(self, url, **kwargs):
+        if getattr(self, 'API_BASE_URL', None):
+            kwargs['api_base_url'] = self.API_BASE_URL
+        return self._client.get(url, **kwargs)
+
+    def _post(self, url, **kwargs):
+        if getattr(self, 'API_BASE_URL', None):
+            kwargs['api_base_url'] = self.API_BASE_URL
+        return self._client.post(url, **kwargs)
+
+    @property
+    def access_token(self):
+        return self._client.access_token
+
+    @property
+    def session(self):
+        return self._client.session
--- wechat_django/sdk/client/user.py
+++ wechat_django/sdk/client/user.py
@@ -16,68 +16,68 @@ class WeChatUser(BaseWeChatAPI):
         :param lang: 国家地区语言
         :return:返回json数据包
         """
-        # return self._get(
-        #     'user/info',
-        #     params={
-        #         'openid': user_id,
-        #         'lang': lang
-        #         }
-        # )
-        pass
-    #
-    # def get_followers(self, first_user_id=None):
-    #     """
-    #         获取关注者列表，微信服务器会保存关注者列表
-    #         参考：http://mp.weixin.qq.com/wiki/3/17e6919a39c1c53555185907acf70093.html
-    #     :param firest_user_id:第一个拉取的用户id
-    #     :return:返回的json包
-    #     """
-    #     params = {}
-    #     if first_user_id:
-    #         params['next_openid'] = first_user_id
-    #     return self._get(
-    #         'user/get',
-    #         params=params
-    #     )
-    #
-    # def update_remark(self, user_id, remark):
-    #     """
-    #         设置用户备注名字
-    #         http://mp.weixin.qq.com/wiki/10/bf8f4e3074e1cf91eb6518b6d08d223e.html
-    #     :param user_id:用户id
-    #     :param remark:备注名
-    #     :return:返回的json数据包
-    #     """
-    #     return self._post(
-    #         'uer/info/updateremark',
-    #         data={
-    #             'openid': user_id,
-    #             'remark': remark
-    #         }
-    #     )
-    #
-    # def get_group_id(self, user_id):
-    #     """
-    #     获取用户所在分组
-    #     :param user_id:
-    #     :return:
-    #     """
-    #     res = self._post(
-    #         'groups/getid',
-    #         data={'openid': user_id},
-    #         result_processor=lambda x: x['groupid']
-    #     )
-    #     return res
-    #
-    # def get_batch(self, user_list):
-    #     """
-    #         批量获取用户基本信息
-    #     :param user_list:
-    #     :return:用户信息列表list对象
-    #     """
-    #     res = self._post(
-    #         'user/info/batchget',
-    #         data={'user_list': user_list},
-    #         result_processor=lambda x: x['user_info_list']
-    #     )
-    #     return res
+        return self._get(
+            'user/info',
+            params={
+                'openid': user_id,
+                'lang': lang
+                }
+        )
+
+
+    def get_followers(self, first_user_id=None):
+        """
+            获取关注者列表，微信服务器会保存关注者列表
+            参考：http://mp.weixin.qq.com/wiki/3/17e6919a39c1c53555185907acf70093.html
+        :param firest_user_id:第一个拉取的用户id
+        :return:返回的json包
+        """
+        params = {}
+        if first_user_id:
+            params['next_openid'] = first_user_id
+        return self._get(
+            'user/get',
+            params=params
+        )
+
+    def update_remark(self, user_id, remark):
+        """
+            设置用户备注名字
+            http://mp.weixin.qq.com/wiki/10/bf8f4e3074e1cf91eb6518b6d08d223e.html
+        :param user_id:用户id
+        :param remark:备注名
+        :return:返回的json数据包
+        """
+        return self._post(
+            'uer/info/updateremark',
+            data={
+                'openid': user_id,
+                'remark': remark
+            }
+        )
+
+    def get_group_id(self, user_id):
+        """
+        获取用户所在分组
+        :param user_id:
+        :return:
+        """
+        res = self._post(
+            'groups/getid',
+            data={'openid': user_id},
+            result_processor=lambda x: x['groupid']
+        )
+        return res
+
+    def get_batch(self, user_list):
+        """
+            批量获取用户基本信息
+        :param user_list:
+        :return:用户信息列表list对象
+        """
+        res = self._post(
+            'user/info/batchget',
+            data={'user_list': user_list},
+            result_processor=lambda x: x['user_info_list']
+        )
+        return res
