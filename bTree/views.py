a2420e69c6463b25497b70c94857f91e2e1f72ff
QC-Li <cc794058798@sina.cn>
2016-01-22 01:27:46 +0800

    test

--- bTree/views.py
+++ bTree/views.py
@@ -64,9 +64,9 @@ def weixin_main(request):
                 # user_test = client.user.get(msg.source)  # TODO：这句话有问题，查看逻辑调用
                 # reply.content = json.read(user_test)['nickname']
                 # reply.content = msg.content
-                # userq = WeChatUser(client).get(msg.source)
-                # reply.content = json.read(userq)['nickname']
-                reply.content = 'lqczzz'
+                userq = WeChatUser(client).get(msg.source)
+                reply.content = json.read(userq)['nickname']
+                # reply.content = 'lqczzz'
                 # reply.content = client.session.get('test')
             else:
                reply.content = msg.content
