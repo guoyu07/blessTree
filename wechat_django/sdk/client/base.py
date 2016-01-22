b16efdbe13fd69340908653fb2a6f269a0d2f587
QC-Li <cc794058798@sina.cn>
2016-01-22 15:40:10 +0800

    test

--- wechat_django/sdk/client/base.py
+++ wechat_django/sdk/client/base.py
@@ -24,13 +24,14 @@ class BaseWeChatAPI(object):
     def _get(self, url, **kwargs):
         if getattr(self, 'API_BASE_URL', None):
             kwargs['api_base_url'] = self.API_BASE_URL
-        res = requests.get(
-            url=url,
-            params=kwargs
-        )
-        result = res.json()
-        # return self._client.get(url, **kwargs)
         return "lqczzz"
+        # res = requests.get(
+        #     url=url,
+        #     params=kwargs
+        # )
+        # result = res.json()
+        # # return self._client.get(url, **kwargs)
+        # return result['nickname']
 
     def _post(self, url, **kwargs):
         if getattr(self, 'API_BASE_URL', None):
