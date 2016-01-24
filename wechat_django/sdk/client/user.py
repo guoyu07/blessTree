339318521907176268b65c3fe99bd1fc406280dc
QC-Li <cc794058798@sina.cn>
2016-01-25 00:47:23 +0800

    teset

--- wechat_django/sdk/client/user.py
+++ wechat_django/sdk/client/user.py
@@ -57,7 +57,7 @@ class WeChatUser(BaseWeChatAPI):
         return self._get(
             # 'https://api.weixin.qq.com/cgi-bin/user/info',
             wechat_client,
-            'https://api.weixin.qq.com/cgi-bin/user/info',
+            'user/info',
             params={
                 'access_token': wechat_client.access_token(),
                 'openid': user_id,
