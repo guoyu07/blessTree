04d308d702ac06e72e8f5fb091f3b7c593dc54f7
QC-Li <cc794058798@sina.cn>
2016-01-24 20:57:15 +0800

    teset

--- wechat_django/sdk/client/user.py
+++ wechat_django/sdk/client/user.py
@@ -64,7 +64,7 @@ class WeChatUser(BaseWeChatAPI):
         #         'lang': lang
         #         }
         # )
-        return client.access_token()
+        return client
 
     def get_followers(self, access_token, first_user_id=None):
         """
