4b973cbebc6e2ac6dbc5ed032b440efa3ba61661
QC-Li <cc794058798@sina.cn>
2016-01-29 17:03:46 +0800

    test

--- wechat_django/sdk/client/menu.py
+++ wechat_django/sdk/client/menu.py
@@ -63,12 +63,12 @@ class WeChatMenu(BaseWeChatAPI):
         :param menu_data:python字典，菜单的信息
         :return:json数据
         """
-        # return self._post(
-        #     client,
-        #     'menu/create',
-        #     params=menu_data
-        # )
-        return "lqczzz"
+        return self._post(
+            client,
+            'menu/create',
+            params=menu_data
+        )
+
 
     update = create
 
