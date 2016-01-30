b5055f43085c1127bee2c3648debe5cb4afa6228
QC-Li <cc794058798@sina.cn>
2016-01-31 01:36:23 +0800

    test

--- bTree/views.py
+++ bTree/views.py
@@ -55,9 +55,9 @@ def weixin_main(request):
             # 消息自动回复
             if msg.content == '我':
                 # client = WeChatClient(appId, appsecret)
-                reply.content = client.fetch_access_token()  # 这句话必须有，先获取接口api调用权限
-                # user = client.user.get(client, msg.source)
-                # reply.content = user['nickname']
+                client.fetch_access_token()  # 这句话必须有，先获取接口api调用权限
+                user = client.user.get(client, msg.source)
+                reply.content = user['nickname']
 
 
             # elif msg.content == '分享':
--- wechat_django/sdk/client/base.py
+++ wechat_django/sdk/client/base.py
@@ -18,8 +18,8 @@ class BaseWeChatAPI(object):
 
     API_BASE_URL = 'https://api.weixin.qq.com/cgi-bin/'
 
-    def __init__(self, client=None):
-        self._client = client
+    # def __init__(self, client=None):
+    #     self._client = client
 
     # def _get(self, url, params):
     #     res = requests.get(
@@ -42,13 +42,13 @@ class BaseWeChatAPI(object):
             url = self.API_BASE_URL + url + "?access_token=" + wechat_client.access_token
         return wechat_client.post(url, data)
 
-    @property
-    def access_token(self):
-        return self._client.access_token
-
-    @property
-    def session(self):
-        return self._client.session
+    # @property
+    # def access_token(self):
+    #     return self._client.access_token
+    #
+    # @property
+    # def session(self):
+    #     return self._client.session
 
 
 def _is_api_endpoint(obj):
