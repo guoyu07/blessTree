626de8077ea2e63a0a86897f70f1146687a3e692
QC-Li <cc794058798@sina.cn>
2016-02-03 03:34:44 +0800

    finally..

--- wechat_django/sdk/oauth.py
+++ wechat_django/sdk/oauth.py
@@ -124,7 +124,7 @@ class WeChatOAuth(object):
             else:
                 del global_code[code]
         else:
-            global_code[code] = 17
+            global_code[code] = 170
             return self._fetch_access_token(code)
 
 
