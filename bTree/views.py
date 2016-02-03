2fe9018cafefe7bbf826780031e4763da27d7464
QC-Li <cc794058798@sina.cn>
2016-02-03 16:54:44 +0800

    finally..

--- bTree/views.py
+++ bTree/views.py
@@ -292,11 +292,11 @@ def ajax_distribute(request):
         user_name = request.POST.get('nickname', '')
         tree_name = request.POST.get('tree_name', '')
         if user_id and user_name and tree_name:
-            user = User(openid=user_id, nickname=user_name, time_stamp=time.time(), tree_name=tree_name)
-            tree = Tree(owner=user, tree_name=tree_name, type=7, action_time=time.time(), read=True, source_id=user_id,
-                        content=u'创建了祝福树')
-            user.save()
-            tree.save()
+            # user = User(openid=user_id, nickname=user_name, time_stamp=time.time(), tree_name=tree_name)
+            # tree = Tree(owner=user, tree_name=tree_name, type=7, action_time=time.time(), read=True, source_id=user_id,
+            #             content=u'创建了祝福树')
+            # user.save()
+            # tree.save()
             ret = '1'
         else:
             ret = '2'
