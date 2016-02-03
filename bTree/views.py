d82b3614289cbb1b5415547dac09740b9af6d66d
QC-Li <cc794058798@sina.cn>
2016-02-03 16:53:41 +0800

    finally..

--- bTree/views.py
+++ bTree/views.py
@@ -286,7 +286,22 @@ def ajax_distribute(request):
     #     response.write(ret)
     #     return response
     if ajax_type == '1':
-        ajax_1(ajax_type)
+        response = HttpResponse()
+        response['Content-Type'] = 'text/javascript'
+        user_id = request.POST.get('openid', '')
+        user_name = request.POST.get('nickname', '')
+        tree_name = request.POST.get('tree_name', '')
+        if user_id and user_name and tree_name:
+            user = User(openid=user_id, nickname=user_name, time_stamp=time.time(), tree_name=tree_name)
+            tree = Tree(owner=user, tree_name=tree_name, type=7, action_time=time.time(), read=True, source_id=user_id,
+                        content=u'创建了祝福树')
+            user.save()
+            tree.save()
+            ret = '1'
+        else:
+            ret = '2'
+        response.write(ret)
+        return response
     else:
         response = HttpResponse()
         response['Content-Type'] = 'text/javascript'
