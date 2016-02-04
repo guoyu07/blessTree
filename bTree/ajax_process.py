4bfbbb7981c4ce32846016a37e4a79082b028e8b
QC-Li <cc794058798@sina.cn>
2016-02-05 02:17:20 +0800

    ajax-2

--- bTree/ajax_process.py
+++ bTree/ajax_process.py
@@ -298,7 +298,7 @@ def ajax_9(request):
         user = User.objects.get(openid=user_id)
         bless = Tree(owner=user, tree_name=user.tree_name, type=5, action_time=time.time(), source_id=source_id,
                      content=bless_con)
-        # bless.save()
+        bless.save()
         # source_user = User.objects.get(openid=source_id)
         # source_user.count = source_user.count + 5000
         # source_user.save()
