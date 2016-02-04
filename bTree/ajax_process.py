a2e404099778d71cfee94788567ce879d3b7ceed
QC-Li <cc794058798@sina.cn>
2016-02-05 02:27:03 +0800

    ajax-2

--- bTree/ajax_process.py
+++ bTree/ajax_process.py
@@ -305,7 +305,7 @@ def ajax_9(request):
         # source_user.count = source_user.count + 5000
         # source_user.save()
         ret = '1'
-        name_dict = [{"name": bless}, {'name': user_id}, {'name': bless_con}]
+        name_dict = [{"name": user}, {'name': user_id}, {'name': bless_con}]
         json_dict = json.dumps(name_dict)
         response['Content-Type'] = 'application/json'
         response.write(json_dict)
