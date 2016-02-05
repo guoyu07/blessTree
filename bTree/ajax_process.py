35d0e58bb25715e9b882b19de33df87345a15c64
QC-Li <cc794058798@sina.cn>
2016-02-05 21:30:54 +0800

    fix init

--- bTree/ajax_process.py
+++ bTree/ajax_process.py
@@ -52,20 +52,20 @@ def ajax_1(request):
     user_name = request.POST.get('nickname', '')
     tree_name = request.POST.get('tree_name', '')
     if user_id and user_name and tree_name:
-    #     try:
-    #         user = User.objects.get(openid=user_id, tree_name='na')
-    #         user.is_plant = True
-    #         user.friends.count = user.friends.count + 20000
-    #         user.save()
-    #         msg = Tree(owner=user.friends, tree_name=user.friends.tree_name, type=4, action_time=time.time(),
-    #                    read=False, source_id=user_id, content='创建了祝福树')
-    #         msg.save()
-    #     except ObjectDoesNotExist:
-    #         user = User(openid=user_id, nickname=user_name, time_stamp=time.time(), tree_name=tree_name)
-    #         user.save()
-    #     tree = Tree(owner=user, tree_name=tree_name, type=7, action_time=time.time(), read=True, source_id=user_id,
-    #                 content='创建了祝福树')
-    #     tree.save()
+        try:
+            user = User.objects.get(openid=user_id, tree_name='na')
+            user.is_plant = True
+            # user.friends.count = user.friends.count + 20000
+            user.save()
+            msg = Tree(owner=user.friends, tree_name=user.friends.tree_name, type=4, action_time=time.time(),
+                       read=False, source_id=user_id, content='创建了祝福树')
+            msg.save()
+        except ObjectDoesNotExist:
+            user = User(openid=user_id, nickname=user_name, time_stamp=time.time(), tree_name=tree_name)
+            user.save()
+        tree = Tree(owner=user, tree_name=tree_name, type=7, action_time=time.time(), read=True, source_id=user_id,
+                    content='创建了祝福树')
+        tree.save()
         ret = '1'
     else:
         ret = '2'
