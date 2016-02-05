36df03cbd379cb0609c3c014b8db7f959ff2335b
QC-Li <cc794058798@sina.cn>
2016-02-06 02:34:21 +0800

    fix init

--- bTree/ajax_process.py
+++ bTree/ajax_process.py
@@ -95,21 +95,21 @@ def ajax_2(request):
         try:
             user_list = user.friends.filter(is_plant=True)[0]
             user_list = user.friends.filter(is_plant=True).order_by('-count')[load_begin:load_begin+7]
+            user_dict = []
+            for user in user_list:
+                # 获取用户头像
+                client.fetch_access_token()
+                user_info = client.user.get(client, user_id)
+                # 生成用户页面访问链接
+                user_home = WeChatOAuth(appId, appsecret,
+                                    'http://1.blesstree.sinaapp.com/wechat/visit'+'?openid='+user.openid).authorize_url
+                # 生成传输用的数据
+                user_dict.append({"name": user.nickname,
+                                  "avatar": user_info['headimgurl'],
+                                  "count": user.count,
+                                  "user_home": user_home})
         except IndexError:
             user_list = []
-        # user_dict = []
-        # for user in user_list:
-        #     # 获取用户头像
-        #     client.fetch_access_token()
-        #     user_info = client.user.get(client, user_id)
-        #     # 生成用户页面访问链接
-        #     user_home = WeChatOAuth(appId, appsecret,
-        #                         'http://1.blesstree.sinaapp.com/wechat/visit'+'?openid='+user.openid).authorize_url
-        #     # 生成传输用的数据
-        #     user_dict.append({"name": user.nickname,
-        #                       "avatar": user_info['headimgurl'],
-        #                       "count": user.count,
-        #                       "user_home": user_home})
         response['Content-Type'] = 'application/json'
         user_dict = [{"name": '启程'}, {'name': "标"}, {'name': "啦啦啦"}]
         json_dict = json.dumps(user_dict)
