c8a283eb08f84d77c133cde1a308a28f97955a74
QC-Li <cc794058798@sina.cn>
2016-02-05 21:31:40 +0800

    fix init

--- bTree/ajax_process.py
+++ bTree/ajax_process.py
@@ -63,9 +63,9 @@ def ajax_1(request):
         except ObjectDoesNotExist:
             user = User(openid=user_id, nickname=user_name, time_stamp=time.time(), tree_name=tree_name)
             user.save()
-        tree = Tree(owner=user, tree_name=tree_name, type=7, action_time=time.time(), read=True, source_id=user_id,
-                    content='创建了祝福树')
-        tree.save()
+        # tree = Tree(owner=user, tree_name=tree_name, type=7, action_time=time.time(), read=True, source_id=user_id,
+        #             content='创建了祝福树')
+        # tree.save()
         ret = '1'
     else:
         ret = '2'
