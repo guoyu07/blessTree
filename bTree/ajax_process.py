d33bd4fdebfa404f1136c78cc7a4fdd4c02fe0dc
QC-Li <cc794058798@sina.cn>
2016-02-06 19:28:09 +0800

    fix init

--- bTree/ajax_process.py
+++ bTree/ajax_process.py
@@ -272,7 +272,7 @@ def ajax_7(request):
                                       'will_con': will.content})
                 response['Content-Type'] = 'application/json'
                 json_bless = json.dumps(will_dict, ensure_ascii=False)
-                response.write(json_bless)
+                response.write(will_dict)
                 return response
             except IndexError:
                 will_dict = []
