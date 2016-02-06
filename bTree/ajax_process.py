16c333d2b1d30f6749904b4a49554faa2cbe6cd8
QC-Li <cc794058798@sina.cn>
2016-02-06 19:55:19 +0800

    fix init

--- bTree/ajax_process.py
+++ bTree/ajax_process.py
@@ -268,8 +268,8 @@ def ajax_7(request):
                 will_list = Tree.objects.filter(owner=owner, type=2).order_by('action_time')
                 will_dict = []
                 for will in will_list:
-                    will.action_time.ctime()
-                    will_dict.append({'will_time': will.action_time.ctime(),
+                    time = will.action_time.strftime("%Y-%m-%d")+str(8+int(will.action_time.strftime("%H")))+will.action_time.strftime(":%I:%S")
+                    will_dict.append({'will_time': time,
                                       'will_con': will.content})
                 response['Content-Type'] = 'application/json'
                 json_bless = json.dumps(will_dict)
