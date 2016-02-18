882d31fc0eebb4118590ec3057abb9c41047dde7
QC-Li <cc794058798@sina.cn>
2016-02-19 00:18:34 +0800

    qrcode fix

--- bTree/static/tree/js/jquery.lqc.extension.js
+++ bTree/static/tree/js/jquery.lqc.extension.js
@@ -48,5 +48,51 @@
         parseInt((n-1)*Math.random())
     }
 
+    $.preventDefault = function(e) {
+            e = e || window.event;
+            e.preventDefault && e.preventDefault();
+            e.returnValue = false;
+        }
+
+    $.stopPropagation = function(e){
+        e = e || window.event;
+        e.stopPropagation && e.stopPropagation();
+        e.cancelBubble = false;
+    }
+
+    $.innerScroll = function(e){
+        // 阻止冒泡到document
+        // document上已经preventDefault
+        stopPropagation(e);
+
+        var delta = e.wheelDelta || e.detail || 0;
+        var box = $(this).get(0);
+
+        if($(box).height() + box.scrollTop >= box.scrollHeight){
+            if(delta < 0) {
+                preventDefault(e);
+                return false;
+            }
+        }
+        if(box.scrollTop === 0){
+            if(delta > 0) {
+                preventDefault(e);
+                return false;
+            }
+        }
+        // 会阻止原生滚动
+        // return false;
+    }
+
+    $.disableScroll = function(){
+        $(document).on('mousewheel', preventDefault);
+        $(document).on('touchmove', preventDefault);
+    };
+
+    $.enableScroll = function(){
+        $(document).off('mousewheel', preventDefault);
+        $(document).off('touchmove', preventDefault);
+    };
+
     //
 })(jQuery)
